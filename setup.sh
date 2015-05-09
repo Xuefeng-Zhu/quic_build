22b29dc550c30732ee970ec96c2e32865c16f64a
Xuefeng Zhu <xzhu15@illinois.edu>
2015-05-09 14:37:17 -0500

    set system buffer

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 sudo apt-get update
 sudo apt-get install libnss3 -y
+
+sudo sysctl -w net.core.rmem_max=8388608
+sudo sysctl -w net.core.wmem_max=8388608
+sudo sysctl -w net.core.rmem_default=65536
+sudo sysctl -w net.core.wmem_default=65536
